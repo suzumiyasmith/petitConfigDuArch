d90706b5afea79ee505fc5e03421c94551cc1581
suzumiyasmith <suzumiyasmith@gmail.com>
2017-03-02 00:52:59 +0000

    adding fish rc

---
+++ config.fish
@@ -0,0 +1,28 @@
+function f1
+  fg %1
+end
+
+function f2
+  fg %2
+end
+
+function f3
+  fg %3
+end
+
+function ff
+  terminator --working-directory (realpath .)
+end
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/home/suzumiya/.config/omf"
+
+# Setting vi mode
+  # fish_vi_mode is deperecated
+# fish_vi_key_bindings
+
+#set enviroment pour stack
+#set PATH $PATH /home/suzumiya/.local/bin
+
+# Load oh-my-fish configuration.
+#source $OMF_PATH/init.fish
